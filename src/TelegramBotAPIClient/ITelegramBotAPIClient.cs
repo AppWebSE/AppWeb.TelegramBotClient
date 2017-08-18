68e0cfa70fc378cba812b26ad6fb0a30e69e8222
Mattias Andersson <mattiasandersson88@hotmail.com>
2017-08-18 22:43:14 +0200

    Adjusted client interface

--- src/TelegramBotAPIClient/ITelegramBotAPIClient.cs
+++ src/TelegramBotAPIClient/ITelegramBotAPIClient.cs
@@ -1,9 +1,11 @@
-﻿using TelegramBotAPIClient.Models;
+﻿using TelegramBotAPIClient.Enums;
+using TelegramBotAPIClient.Models;
 
 namespace TelegramBotAPIClient
 {
     public interface ITelegramBotAPIClient
     {
         Message SendMessage(int chat_id, string text);
+        Update[] GetMessages(int? offset, int? limit, int? timeout, UpdateType[] allowedUpdates = null);
     }
 }
