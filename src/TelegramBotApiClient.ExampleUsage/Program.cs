486b5eb3d566c44a1631111584b7553fc48764fa
Mattias Andersson <mattiasandersson88@hotmail.com>
2017-08-18 22:51:03 +0200

    Added simple console application as a "running" example of usage

---
+++ src/TelegramBotApiClient.ExampleUsage/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using TelegramBotAPIClient;
+using TelegramBotAPIClient.Enums;
+using TelegramBotAPIClient.Models;
+
+namespace TelegramBotApiClient.ExampleUsage
+{
+    public class Program
+    {
+
+        public Program()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true);
+            var Configuration = builder.Build();
+           
+            var accessToken = Configuration["Settings:accessToken"];
+            int chatId = int.Parse(Configuration["Settings:chatId"]);
+
+            ITelegramBotAPIClient _apiClient = new TelegramBotAPIClient.TelegramBotAPIClient(accessToken);
+
+            Console.WriteLine("Started");
+
+            if(chatId > 0)
+            {
+                _apiClient.SendMessage(chatId, "Test message, did you receive it? :)");
+            }
+
+            Console.WriteLine("Message sent");
+
+            UpdateType[] updateTypes = {
+                UpdateType.All
+            };
+
+            Update[] updates = _apiClient.GetMessages(0, 100, 0, updateTypes);
+
+
+            Console.WriteLine("Messages read");
+        }
+    }
+}
